4e09f7ffe137fb6e7d8c19efb6c0012a95765317
kristen77@gmail.com <kristen77@gmail.com>
2018-07-30 16:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 
+describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(index.message).toInclude(index.name)
